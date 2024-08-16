60c810f039a583c5bec604e35da1195b6cbc18ce
Hara Honasan <honasan0@gmail.com>
2024-08-16 22:23:28 +0800

    removed useEffect from Accounts.jsx

--- src/pages/Accounts.jsx
+++ src/pages/Accounts.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import data from "../assets/data/bankUsers.json";
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
